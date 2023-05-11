2f485bc19aa49ea388209c3043b5a8559cf59c4f
Hassan <99126222+HassanMmd@users.noreply.github.com>
2023-05-11 12:34:57 +0300

    Semantic HTML

--- lemon/src/App.js
+++ lemon/src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import Footer from './Footer';
+import Header from './Header';
+import Main from './Main';
+import Nav from './Nav';
+import React from "react"
 
 function App() {
   return (
     <div className="App">
+      <React.Fragment>
+        <Header />
+        <Nav />
+        <Main />
+        <Footer />
+      </React.Fragment>
     </div>
   );
 }
---
+++ lemon/src/Header.js
@@ -0,0 +1,10 @@
+import logo from "./assets/Logo .svg"
+
+function Header(){
+return (
+<header>
+<img src={logo}></img>
+</header>
+);
+}
+export default Header;
